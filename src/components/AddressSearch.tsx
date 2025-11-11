8174355c8314f67013a8fde2679386fc3726e8f4
Agi <agi.meri"equineregister.co.uk>
2025-11-11 20:07:47 +0000

    last tweaks

--- src/components/AddressSearch.tsx
+++ src/components/AddressSearch.tsx
@@ -11,13 +11,13 @@ export function AddressSearch({
   setInputValue: Dispatch<SetStateAction<string>>
 }) {
   return (
-    <div className="flex max-md:flex-col gap-2 md:min-w-[450px] max-w-[600px]">
+    <div className="flex max-md:flex-col gap-2 sm:gap-4 md:min-w-[450px] max-w-[600px]">
       <input
         type="text"
         placeholder="Enter Ethereum address"
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
-        className="w-full p-2 border rounded-sm text-sm"
+        className="w-full p-2 border border-gray-400 rounded-sm text-sm"
       />
       <button
         onClick={() => onSearch(inputValue)}
